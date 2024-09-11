46387ead4b1f392dc84cbaaa7d744053db505717
arthurgranito <163346646+arthurgranito@users.noreply.github.com>
2024-09-11 14:38:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const nome = document.getElementById('nome');
+const nota1 = document.getElementById('nota1');
+const nota2 = document.getElementById('nota2');
+const form = document.getElementById('form');
+const resultado = document.getElementById('resultado');
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    if(nome.value == '' || nota1.value == '' || nota2.value == ''){
+        alert('Preencha os campos!');
+    } else{
+        const notaFinal = ((36 - parseFloat(nota1.value) - (2 * parseFloat(nota2.value)))/3).toFixed(1);
+
+
+        const h3 = document.createElement('h3');
+        h3.innerText = `${nome.value}, para passar de ano você precisa tirar ${notaFinal}`;
+        resultado.appendChild(h3);
+
+        form.reset()
+    }
+})
